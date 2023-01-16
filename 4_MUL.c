b039b68ea9605d2ccf89fc865afa6f4f130220e5
Farid Abdi <bngfm97@gmail.com>
2023-01-16 20:44:54 +0100

    multiplication

---
+++ 4_MUL.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /***
+     * multipication de deux nombres entiers entrés en clavier
+     * en utilisant 3 variable
+     */
+
+    int a;
+    int b;
+    int resultat;
+
+    printf("Entrer le nombre a : ");
+    scanf("%d", &a);
+
+    printf("Entrer le nombre b : ");
+    scanf("%d", &b);
+
+    resultat = a * b;
+
+    printf("%d * %d = %d", a, b, resultat);
+
+    return 0;
+}
